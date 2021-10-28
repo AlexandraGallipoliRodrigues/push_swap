0eb44eb74a41d1ce9775029c637121c5e560117f
Alexandra Gallipoli <83506452+agallipo@users.noreply.github.com>
2021-10-28 14:16:19 +0200

    0987654321

--- big_algorithm.c
+++ big_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: agallipo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 20:28:31 by agallipo          #+#    #+#             */
-/*   Updated: 2021/10/26 13:43:36 by agallipo         ###   ########.fr       */
+/*   Updated: 2021/10/28 14:14:59 by agallipo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,6 @@ int	*ft_organised_array(t_list	*stack_a)
 
 	size = ft_check_nums(&stack_a);
 	temp = stack_a;
-	printf("size = %i\n", size);
 	org = malloc(sizeof(int) * size);
 	i = 0;
 	while (i < size && temp)
@@ -79,30 +78,49 @@ int	ft_median(t_list *stack_a)
 	return (median);
 }
 
+static void	ft_loop(t_list **stack_a, t_list **stack_b, int pos,int *org)
+{
+	t_list	*temp;
+	int		i;
+
+	i = ft_check_nums(stack_a);
+	temp = *stack_a;
+	printf("ORG[POS] : %i\n", org[pos]);
+	while (i >= 0 && temp)	{
+		if (temp->content <= org[pos])
+		{
+			temp= temp->next;
+			ft_pb(stack_a, stack_b);
+		}
+		else
+		{
+			temp = temp->next;
+			ft_ra(stack_a);
+		}
+		i--;
+	}
+}
+
 void	ft_big(t_list **stack_a, t_list **stack_b, int chk)
 {
 	int		pos;
 	int 	size;
 	int		*org;
-	t_list	*temp;
-	int		i;
 
+	org = ft_organised_array(*stack_a);
 	size = ft_check_nums(stack_a);
 	pos = size/chk;
-	org = ft_organised_array(*stack_a);
-	printf("pos: %i\n org: %i\n", pos, org[pos]);
-	temp = *stack_a;
-	printf("hola\n");
-	while (i <= 100)
+	while (chk > 0)
 	{
-		printf("hola\n");
-		if (temp->content <= org[pos])
-			ft_pb(stack_a, stack_b);
-		else
-			ft_ra(stack_a);
-		temp = temp->next;
-
+		printf("POSICION: %i\nSIZE: %i\n", pos, size);
+		ft_loop(stack_a, stack_b, pos, org);
+		pos += pos;
+		if (pos > size)
+		{
+			printf("HOLA\n");
+			pos = size - 1;	
+		}
+		chk--;
 	}
 	ft_print_lst(stack_a, stack_b);
 }
-
